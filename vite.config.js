13f34db51d4ee4c570cf3e27f3e0104eaa94236e
bobits <bobkeri.gabule@gmail.com>
2025-06-04 12:08:27 +0800

    add vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
+  
 });
